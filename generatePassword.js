6ebac0c0816314d73bff89a4fd0e5b5ad7eb4d7f
alexlex <ozziepeck11@gmail.com>
2017-10-02 19:09:24 +0100

    package.json

--- generatePassword.js
+++
@@ -1,7 +0,0 @@
-const bCrypt = require('bCrypt');
-const numberOfRounds = 3;
-const password = 'password'; //OH MY FUCKING GOD.
-const salt = bCrypt.genSaltSync(numberOfRounds);
-const encryptedPassword = bCrypt.hashSync(password, salt);
-console.log(salt);
-console.log(encryptedPassword);
