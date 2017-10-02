35d12ff6c1320738105a3b0d77520146e4e7631c
alexlex <ozziepeck11@gmail.com>
2017-10-02 19:07:30 +0100

    OH MY GOD! Sorted.

--- generatePassword.js
+++ generatePassword.js
@@ -1,10 +1,7 @@
 const bCrypt = require('bCrypt');
 const numberOfRounds = 3;
-const password = 'Cryptographia1';
-//const salt = bCrypt.genSaltSync(numberOfRounds);
-const encryptedPassword = bCrypt.hashSync(
-  password,
-  '$2a$04$b.dbXA8S8XjGfqyn4XvuXO'
-);
-
+const password = 'password'; //OH MY FUCKING GOD.
+const salt = bCrypt.genSaltSync(numberOfRounds);
+const encryptedPassword = bCrypt.hashSync(password, salt);
+console.log(salt);
 console.log(encryptedPassword);
